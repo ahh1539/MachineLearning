ce54960a08a1fa59646ba5821dfe128560e69573
ahh1539 <31779206+ahh1539@users.noreply.github.com>
2018-12-04 17:30:49 -0500

    deleted some redundant code

--- machine_learning/irisalgo.py
+++ machine_learning/irisalgo.py
@@ -84,23 +84,6 @@ def machineLearning(dataset):
     seed = 7
     scoring = 'accuracy'
     # Spot Check Algorithms
-    models = []
-    models.append(('LR', LogisticRegression()))
-    models.append(('LDA', LinearDiscriminantAnalysis()))
-    models.append(('KNN', KNeighborsClassifier()))
-    models.append(('CART', DecisionTreeClassifier()))
-    models.append(('NB', GaussianNB()))
-    models.append(('SVM', SVC()))
-    # evaluate each model in turn
-    results = []
-    names = []
-    for name, model in models:
-        kfold = model_selection.KFold(n_splits=10, random_state=seed)
-        cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
-        results.append(cv_results)
-        names.append(name)
-        msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-        print(msg)
     print("ML MODELS:")
     models = []
     models.append(('LR', LogisticRegression()))
@@ -119,6 +102,7 @@ def machineLearning(dataset):
         names.append(name)
         msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
         print(msg)
+
 
 def main():
     # This loads the dataset from the online line
