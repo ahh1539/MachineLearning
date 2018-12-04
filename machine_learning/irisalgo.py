2a54c30cda016a4fe9cf201263ccbf08b6d05f18
ahh1539 <31779206+ahh1539@users.noreply.github.com>
2018-12-04 15:10:20 -0500

    organizing into functions

--- machine_learning/irisalgo.py
+++ machine_learning/irisalgo.py
@@ -39,45 +39,31 @@ from sklearn.svm import SVC
 #url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 
 
-# This loads the dataset from the online line
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-f = open("iris.csv")
-dataset = pandas.read_csv(f, names=names)
 
 
+def statistics(dataset):
+    # shape
+    print(dataset.shape)
 
-"""
-Prints out the number of data sets rows and the number
-of categories in each data set as columns (rows, columns)
-"""
-# shape
-print(dataset.shape)
+    # shape, (rows and colums)
+    print(dataset.shape)
 
-"""
-Prints out the first x amount of rows of data
-"""
-# head
-x = 20
-print(dataset.head(x))
+    #Prints out the different classifications of the dataset
+    # class distribution
+    print(dataset.groupby('class').size())
 
-"""
-Prints out the statistical data for the dataset
-"""
-# descriptions
-#print(dataset.describe())
+    # Prints our first "x" rows of data
+    x = 20
+    print(dataset.head(x))
 
-"""
-Prints out the different classifications of the dataset
-"""
-# class distribution
-print(dataset.groupby('class').size())
+    # Statistical analysis of data
+    print(dataset.describe())
 
-"""
-Box and whisker graphs of dataset
-"""
-# box and whisker plots
-#dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-#plt.show()
+    # box and whisker plots
+    dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+    plt.show()
+
+
 
 """
 Training data set 80% used to train, 20% used to test accuracy
@@ -90,3 +76,11 @@ validation_size = 0.20
 seed = 7
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 
+def main():
+    # This loads the dataset from the online line
+    names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+    f = open("iris.csv")
+    dataset = pandas.read_csv(f, names=names)
+    statistics(dataset)
+
+def if __name__ == '__main__':
