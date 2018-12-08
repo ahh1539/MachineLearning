8395ab87392dae043c724fe4f355c41cc5581f20
ahh1539 <31779206+ahh1539@users.noreply.github.com>
2018-12-08 13:55:24 -0500

    very simple example

--- machine_learning/IrisDecisionTree.py
+++ machine_learning/IrisDecisionTree.py
@@ -11,6 +11,7 @@ from sklearn import tree
 
 iris = load_iris()
 
+
 print(iris.feature_names)
 print(iris.data[0])
 print(iris.target_names)
@@ -22,6 +23,7 @@ test_idx = [0, 50, 100]
 #training data
 train_target = np.delete(iris.target, test_idx)
 train_data = np.delete(iris.data, test_idx, axis=0)
+
 
 #testing data
 test_target = iris.target[test_idx]
